c0249349e4a72620a64161183dbf7ef28a986752
Dima Kharitonov <hardi.programm@gmail.comhardi.programm@gmail.com>
2022-11-16 01:00:20 +0300

    implement empty screen

--- simple_todo/lib/presentation/feature/todos/todos_screen.dart
+++ simple_todo/lib/presentation/feature/todos/todos_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '/domain/todo_list.dart';
 
 /// Displays the list of all the todo items.
 class TodosScreen extends StatelessWidget {
@@ -6,6 +9,10 @@ class TodosScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final todo = Provider.of<TodoList>(
+      context,
+      listen: false,
+    );
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -19,6 +26,20 @@ class TodosScreen extends StatelessWidget {
           ),
         ],
       ),
+      body: todo.todolist.isEmpty
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Here is empty list!\nAdd your first To Do!'),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.add),
+                  ),
+                ],
+              ),
+            )
+          : 
     );
   }
 }
