24a8a06ccdec09870086a61ac2b349cc6b91ec0c
Dima Kharitonov <hardi.programm@gmail.comhardi.programm@gmail.com>
2022-11-17 01:35:02 +0300

    implemented Dismissible for todo model

--- simple_todo/lib/presentation/widgets/todo_tile.dart
+++ simple_todo/lib/presentation/widgets/todo_tile.dart
@@ -10,38 +10,79 @@ class TodoTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<TodoModel>(
-      builder: (_, todo, ch) => Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
-        margin: const EdgeInsets.all(5),
-        child: ListTile(
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute<void>(
-                builder: (BuildContext context) => TodoScreen(model: todo),
+      builder: (_, todo, ch) => Dismissible(
+        key: ValueKey(todo.id),
+        confirmDismiss: (direction) => showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: const Text('Delete?'),
+            content: const Text('Do you want to delete this todo?'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(false);
+                },
+                child: const Text('No'),
               ),
-            );
-          },
-          leading: FittedBox(
-            child: Checkbox(
-              fillColor: MaterialStateColor.resolveWith(
-                (states) => Theme.of(context).colorScheme.primary,
-              ),
-              value: todo.isComplete,
-              onChanged: (_) {
-                todo.toggleCompleteStatus();
-              },
-            ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(true);
+                },
+                child: const Text('Yes'),
+              )
+            ],
           ),
-          title: Text(
-            todo.title,
-            style: todo.isComplete
-                ? const TextStyle(
-                    decoration: TextDecoration.lineThrough,
-                    fontSize: 20,
-                  )
-                : const TextStyle(fontSize: 20),
+        ),
+        background: Container(
+          color: Theme.of(context).errorColor,
+          child: const Icon(
+            Icons.delete,
+            color: Colors.white,
+            size: 40,
+          ),
+          alignment: Alignment.centerRight,
+          padding: EdgeInsets.only(right: 20),
+          margin: const EdgeInsets.symmetric(
+            horizontal: 15,
+            vertical: 4,
+          ),
+        ),
+        direction: DismissDirection.endToStart,
+        onDismissed: (direction) =>
+            Provider.of<TodoList>(context, listen: false).deleteTodo(todo.id),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          margin: const EdgeInsets.all(5),
+          child: ListTile(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute<void>(
+                  builder: (BuildContext context) => TodoScreen(model: todo),
+                ),
+              );
+            },
+            leading: FittedBox(
+              child: Checkbox(
+                fillColor: MaterialStateColor.resolveWith(
+                  (states) => Theme.of(context).colorScheme.primary,
+                ),
+                value: todo.isComplete,
+                onChanged: (_) {
+                  todo.toggleCompleteStatus();
+                },
+              ),
+            ),
+            title: Text(
+              todo.title,
+              style: todo.isComplete
+                  ? const TextStyle(
+                      decoration: TextDecoration.lineThrough,
+                      fontSize: 20,
+                    )
+                  : const TextStyle(fontSize: 20),
+            ),
           ),
         ),
       ),
